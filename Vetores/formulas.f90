0508b0c7c88488e423ba767f455743db91b677d8
SageScroll18144 <lipe.fssantos063@gmail.com>
2022-06-22 11:09:10 -0300

    produto escalar

--- Vetores/formulas.f90
+++ Vetores/formulas.f90
@@ -1,14 +1,26 @@
 program formulas
 implicit none
 
-real, dimension(4) :: vector
+real, dimension(1) :: vector
+real, dimension(1) :: vector1
 integer::a=100
 vector = 2
+vector1 = 2
 
-write(*,*) get_norma(vector)
+write(*,*) get_norma(vector), scale_product(vector, vector1)
 call print_vet
 
 contains 
+	
+	subroutine print_vet
+	implicit none
+		integer :: i
+		do i=1,size(vector)
+			write(*,*) vector(i)
+		end do
+		
+	end subroutine print_vet
+	!-------------------------------------------------
 	real function get_norma(vet)
 	implicit none
 		real, dimension(:) :: vet
@@ -20,14 +32,18 @@ contains
 		end do
 		get_norma = sqrt(get_norma)
 	end function get_norma
-	
-	subroutine print_vet
-	implicit none
-		integer :: i
-		do i=1,size(vector)
-			write(*,*) vector(i)
-		end do
+	!-------------------------------------------------
+	real function scale_product(u, v)
+	implicit none 
+		real, dimension(:) :: u
+		real, dimension(:) :: v
 		
-	end subroutine print_vet
-	
+		integer :: i
+		scale_product = 0
+		if (size(u) == size(v)) then
+			do i=1, size(u)
+				scale_product = scale_product + u(i)*v(i)				
+			enddo
+		endif
+	end function scale_product
 end program
